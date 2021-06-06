e308e22e66d2b39263c228d665e6f2454159fc3d
Kcheonwoong <cheonwoong91@gmail.com>
2021-06-07 00:12:39 +0900

    add git url

--- deco_helper/__init__.py
+++ deco_helper/__init__.py
@@ -3,5 +3,6 @@ from .helpers import helper
 __version__ = '0.0.1'
 __author__ = 'CheonwoongKwak'
 __contact__ = 'cheonwoong91@gmail.com'
+__url__ = 'https://github.com/Kcheonwoong/deco_helper'
 
 __all__ = ['helper']
